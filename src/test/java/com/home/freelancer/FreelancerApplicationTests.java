52ffb3fc1b0eef6db51d39c3d0bd5f27a6cf2128
wsalloum <waseem.sy@gmail.com>
2024-10-23 17:27:10 +0200

    Initial commit

---
+++ src/test/java/com/home/freelancer/FreelancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.freelancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
